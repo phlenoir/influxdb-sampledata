67633e82e0d4b63f7addf5e92865e8ed22a88a8f
phlenoir <philippe.lenoir@prologism.fr>
2017-06-27 15:42:34 +0000

    improved time series

--- influxdb/init_influxdb.bash
+++ influxdb/init_influxdb.bash
@@ -6,6 +6,8 @@ DBNAME=trading
 curl -i -XPOST http://$DBURL/query --data-urlencode "q=CREATE DATABASE trading"
 
 # create retention policies
-curl -i -XPOST http://$DBURL/query --data-urlencode "q=CREATE RETENTION POLICY \"rp_unit\" ON \"trading\" DURATION 1h REPLICATION 1 DEFAULT"
-curl -i -XPOST http://$DBURL/query --data-urlencode "q=CREATE RETENTION POLICY \"rp_agg\" ON \"trading\" DURATION 2h REPLICATION 1"
+curl -i -XPOST http://$DBURL/query --data-urlencode "q=DROP RETENTION POLICY \"rp_unit\" ON \"trading\""
+curl -i -XPOST http://$DBURL/query --data-urlencode "q=CREATE RETENTION POLICY \"rp_unit\" ON \"trading\" DURATION 4h REPLICATION 1 DEFAULT"
+curl -i -XPOST http://$DBURL/query --data-urlencode "q=DROP RETENTION POLICY \"rp_agg\" ON \"trading\""
+curl -i -XPOST http://$DBURL/query --data-urlencode "q=CREATE RETENTION POLICY \"rp_agg\" ON \"trading\" DURATION 1d REPLICATION 1"
 
